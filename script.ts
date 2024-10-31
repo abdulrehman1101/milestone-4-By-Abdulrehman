8b304f3e1c1f2c1a8357454c8ebb4a32856f8aa8
Abdul Rehman <160239442+abdulrehman1101@users.noreply.github.com>
2024-10-31 16:54:07 +0500

    Add files via upload

--- script.ts
+++ script.ts
@@ -1,6 +1,9 @@
 //listing element
 document.getElementById('resumeForm')?.addEventListener('submit' , function (event) {
     event.preventDefault();
+
+
+    const profilePictureInput = document.getElementById('profilePicture') as HTMLInputElement;
     
     // type assertion
     const nameElement = document.getElementById('name') as HTMLInputElement;
@@ -11,7 +14,7 @@ document.getElementById('resumeForm')?.addEventListener('submit' , function (eve
     const skillsElement = document.getElementById('skills') as HTMLInputElement;
 
     
-    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+    if (profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
         
         const name = nameElement.value;
         const email = emailElement.value;
@@ -20,9 +23,16 @@ document.getElementById('resumeForm')?.addEventListener('submit' , function (eve
         const experience = experienceElement.value;
         const skills = skillsElement.value;
 
+        // picture Elements
+        const profilePictureFile = profilePictureInput.files?.[0]
+        const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+
+
+
     //create resume Output    
         const resumeOutput = `
            <h2>Resume</h2>
+           ${profilePictureURL ? `<img src = "${profilePictureURL} alt ="Profile Picture" class = "profilePicture">` : '' }
            <p><strong>Name:</strong> <span id = "edit-name" class = "editable"> ${name} </span> </p>
            <p><strong>Email:</strong> <span id = "edit-email" class = "editable"> ${email} </span> </p>
            <p><strong>Phone:</strong> <span id = "edit-phone" class = "editable"> ${phone} </span> </p>
@@ -54,16 +64,19 @@ document.getElementById('resumeForm')?.addEventListener('submit' , function (eve
 
 function makeEditable() {
     const editableElements = document.querySelectorAll('.editable');
+
+
     editableElements.forEach(element => {
         element.addEventListener('click' , function(){
             const currentElement = element as HTMLElement;
+
             const currentValue = currentElement.textContent || "";
 
             // replace content
             if (currentElement.tagName === "P" || currentElement.tagName === "SPAN") {
                 const input = document.createElement('input')
                 input.type = 'text'
-                input.value = currentValue
+                input.value = currentValue;
                 input.classList.add('editing-input')
 
                 input.addEventListener('blur', function(){
